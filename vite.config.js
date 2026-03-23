3f245d36dfdf4a64f6b36d1221dd326b4a8622f4
md7-instructor <media2gather@gmail.com>
2026-03-23 19:36:32 +0900

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+{\rtf1\ansi\ansicpg949\cocoartf2868
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 import \{ defineConfig \} from 'vite'\
+import react from '@vitejs/plugin-react'\
+\
+export default defineConfig(\{\
+  plugins: [react()],\
+\})\
+}
